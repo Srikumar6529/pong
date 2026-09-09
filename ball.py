170809d5ac24b25964a27598b008d46a86595b63
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-09-09 15:38:57 -0500

    ball got speeeeed

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 from circleshape import CircleShape
 from config import *
 class Ball(CircleShape):
@@ -8,4 +8,6 @@ class Ball(CircleShape):
     def draw(self,screen):
         pygame.draw.circle(screen,"white",self.position,self.radius,LINE_WIDTH)
     def update(self,dt):
-        self.position += self.velocity * dt
+        self.velocity = pygame.Vector2(BALL_SPEED,BALL_SPEED)
+        self.velocity.rotate(random.randint(-30, 30))
+        self.position += dt * self.velocity

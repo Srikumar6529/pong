60180835b514f034903df8574af00168099fb7cf
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-09-09 14:14:13 -0500

    added a screen

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import pygame
+from config import *
+
 def main():
     print("Hello from pong!")
     print("Let's build a wonderful pong game")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
 
 
 if __name__ == "__main__":
